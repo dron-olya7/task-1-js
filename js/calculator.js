b4fe19bd8f4404dc596d7939b2d948dc00678c60
Olya Dron <olya_sok00@mail.ru>
2022-02-08 18:28:47 +0500

    Изучила бозовую структуру js

---
+++ js/calculator.js
@@ -0,0 +1,19 @@
+export function sum(a, b) {
+    // your code
+    return a + b
+  }
+  
+  export function subtract(a, b) {
+    // your code
+    return a - b
+  }
+  
+  export function multiply(a, b) {
+    // your code
+    return a * b
+  }
+  
+  export function divide(a, b) {
+    // your code
+    return a / b
+  }
